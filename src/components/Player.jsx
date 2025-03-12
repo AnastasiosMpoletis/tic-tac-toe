2abc78a5938f34b62a13719a9d451a4b1655753c
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-03-12 20:06:09 +0200

    L-75, 77. Add Edit button click handler that switches between input and text components.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,26 @@
+import { useState } from 'react';
+
 /**
  * @param {*} param0 
  * @returns Player component
  */
 export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing(!isEditing);
+    }
+
+    let playerName = isEditing ? <input type="text" required value={name} /> : <span className="player-name">{name}</span>;
+    let buttonCaption = isEditing ? 'Save' : 'Edit';
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {playerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{buttonCaption}</button>
         </li>
     )
 }
